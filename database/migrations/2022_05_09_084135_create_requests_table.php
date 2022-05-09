6b5442a1191e5615e424bf389cbbff7a55eec113
Mohammed Hijazi <mohammedhijazi199@gmail.com>
2022-05-09 11:52:35 +0300

    created database table

---
+++ database/migrations/2022_05_09_084135_create_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('category');
+            $table->enum('quantity_type',['liter','shekel']);
+            $table->unsignedFloat('quantity')->default(0);
+            $table->foreignId('driver_id')->constrained('drivers');
+            $table->enum('status',['on','off']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+};
